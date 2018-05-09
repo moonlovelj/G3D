2d98fe32003a6b26b55a3487810450cafd00011e
guobin <18500056398@163.com>
2018-05-09 14:47:25 +0800

    Add matrix3x3 class
    Add matrix3x3 class

---
+++ common/include/Matrix3x3.h
@@ -0,0 +1,94 @@
+#ifndef __MATRIX3X3_H__
+#define __MATRIX3X3_H__
+
+#include "Vector3D.h"
+#include <ostream>
+namespace g3dcommon
+{
+  class Matrix3x3
+  {
+  public:
+    Matrix3x3():m00(0.f), m01(0.f), m02(0.f),
+                m10(0.f), m11(0.f), m12(0.f),
+                m20(0.f), m21(0.f), m22(0.f) { }
+    
+    Matrix3x3(float m00, float m01, float m02,
+              float m10, float m11, float m12, 
+              float m20, float m21, float m22)
+            : m00(m00), m01(m01), m02(m02),
+              m10(m10), m11(m11), m12(m12), 
+              m20(m20), m21(m21), m22(m22) { }
+
+    Matrix3x3(const Matrix3x3& A)
+            : m00(A.m00), m01(A.m01), m02(A.m02),
+              m10(A.m10), m11(A.m11), m12(A.m12),
+              m20(A.m20), m21(A.m21), m22(A.m22) { }
+
+
+    /**
+    * Returns the 3x3 identity matrix.
+    */
+    static Matrix3x3 identity();
+
+    /**
+    * Returns the determinant of m.
+    */
+    float det() const;
+
+    /**
+    * Returns the transpose of m.
+    */
+    Matrix3x3 T() const;
+
+    /**
+    * Returns the inverse of A.
+    */
+    Matrix3x3 inv() const;
+
+    // accesses element (i,j) of A using 0-based indexing
+          float& operator()(int i, int j);
+    const float& operator()(int i, int j) const;
+
+    // returns -m
+    Matrix3x3 operator-() const;
+
+    // returns m+A
+    Matrix3x3 operator+(const Matrix3x3& A) const;
+
+    // returns m-A
+    Matrix3x3 operator-(const Matrix3x3& A) const;
+
+    // returns m*f
+    Matrix3x3 operator*(float f) const;
+
+    // returns m*A
+    Matrix3x3 operator*(const Matrix3x3& A) const;
+
+    // returns m/f
+    Matrix3x3 operator/(float f) const;
+
+  private:
+    union
+    {
+      float m[3][3];
+      struct
+      {
+        float m00, m01, m02;
+        float m10, m11, m12;
+        float m20, m21, m22;
+      };
+    };
+  };
+
+  // returns v*A
+  Vector3D operator*(const Vector3D& v, const Matrix3x3& A);
+
+  // returns f*A
+  Matrix3x3 operator*(float f, const Matrix3x3& A);
+
+  std::ostream& operator<<(std::ostream& os, const Matrix3x3& A);
+}
+
+
+
+#endif
---
+++ common/src/Matrix3x3.cpp
@@ -0,0 +1,124 @@
+#include "Matrix3x3.h"
+
+namespace g3dcommon {
+
+  Matrix3x3 Matrix3x3::identity()
+  {
+    Matrix3x3 A;
+    A(0, 0) = 1.f; A(0, 1) = 0.f; A(0, 2) = 0.f;
+    A(1, 0) = 0.f; A(1, 1) = 1.f; A(1, 2) = 0.f;
+    A(2, 0) = 0.f; A(2, 1) = 0.f; A(2, 2) = 1.f;
+    return A;
+  }
+
+  float Matrix3x3::det() const
+  {
+    return -m02*m11*m20 + m01*m12*m20 +
+            m02*m10*m21 - m00*m12*m21 -
+            m01*m10*m22 + m00*m11*m22;
+  }
+
+  Matrix3x3 Matrix3x3::T() const
+  {
+    Matrix3x3 A(m00, m10, m20,
+                m01, m11, m21,
+                m02, m12, m22);
+
+    return A;
+  }
+
+  Matrix3x3 Matrix3x3::inv() const
+  {
+    // A is standard adjoint matrix.
+    Matrix3x3 A;
+    A.m00 = m11*m22 - m21*m12; A.m01 = m21*m02 - m01*m22; A.m02 = m01*m12 - m11*m02;
+    A.m10 = m20*m12 - m10*m22; A.m11 = m00*m22 - m20*m02; A.m12 = m10*m02 - m00*m12;
+    A.m20 = m10*m21 - m20*m11; A.m21 = m20*m01 - m00*m21; A.m22 = m00*m11 - m10*m01;
+
+    float rdet = 1.f / det();
+
+    return A * rdet;
+  }
+
+  float& Matrix3x3::operator()(int i, int j)
+  {
+    return m[i][j];
+  }
+
+  const float& Matrix3x3::operator()(int i, int j) const
+  {
+    return m[i][j];
+  }
+
+  Matrix3x3 Matrix3x3::operator-() const
+  {
+    return Matrix3x3(-m00, -m01, -m02,
+                     -m10, -m11, -m12,
+                     -m20, -m21, -m22);
+  }
+
+  Matrix3x3 Matrix3x3::operator+(const Matrix3x3& A) const
+  {
+    return Matrix3x3(m00 + A.m00, m01 + A.m01, m02 + A.m02,
+                     m10 + A.m10, m11 + A.m11, m12 + A.m12,
+                     m20 + A.m20, m21 + A.m21, m22 + A.m22);
+  }
+
+  Matrix3x3 Matrix3x3::operator-(const Matrix3x3& A) const
+  {
+    return Matrix3x3(m00 - A.m00, m01 - A.m01, m02 - A.m02,
+                     m10 - A.m10, m11 - A.m11, m12 - A.m12,
+                     m20 - A.m20, m21 - A.m21, m22 - A.m22);
+  }
+
+  Matrix3x3 Matrix3x3::operator*(float f) const
+  {
+    return Matrix3x3(f*m00, f*m01, f*m02,
+                     f*m10, f*m11, f*m12,
+                     f*m20, f*m21, f*m22);
+  }
+
+  Matrix3x3 Matrix3x3::operator*(const Matrix3x3& A) const
+  {
+    Matrix3x3 M;
+    M.m00 = m00*A.m00 + m01*A.m10 + m02*A.m20; M.m01 = m00*A.m01 + m01*A.m11 + m02*A.m21; M.m02 = m00*A.m02 + m01*A.m12 + m02*A.m22;
+    M.m10 = m10*A.m00 + m11*A.m10 + m12*A.m20; M.m11 = m10*A.m01 + m11*A.m11 + m12*A.m21; M.m12 = m10*A.m02 + m11*A.m12 + m12*A.m22;
+    M.m20 = m20*A.m00 + m21*A.m10 + m22*A.m20; M.m21 = m20*A.m01 + m21*A.m11 + m22*A.m21; M.m22 = m20*A.m02 + m21*A.m12 + m22*A.m22;
+
+    return M;
+  }
+
+  Matrix3x3 Matrix3x3::operator/(float f) const
+  {
+    float rf = 1.f / f;
+    return Matrix3x3(rf*m00, rf*m01, rf*m02,
+                     rf*m10, rf*m11, rf*m12,
+                     rf*m20, rf*m21, rf*m22);
+  }
+
+  Vector3D operator*(const Vector3D& v, const Matrix3x3& A)
+  {
+    return Vector3D(v.x*A(0, 0) + v.y*A(1, 0) + v.z*A(2, 0),
+                    v.x*A(0, 1) + v.y*A(1, 1) + v.z*A(2, 1), 
+                    v.x*A(0, 2) + v.y*A(1, 2) + v.z*A(2, 2));
+  }
+
+  Matrix3x3 operator*(float f, const Matrix3x3& A)
+  {
+    return A*f;
+  }
+
+  std::ostream& operator<<(std::ostream& os, const Matrix3x3& A)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      os << "[";
+      for (int j = 0; j < 3; j++)
+      {
+        os << A(i, j) << ",";
+      }
+      os << "]" << std::endl;
+    }
+    return os;
+  }
+}

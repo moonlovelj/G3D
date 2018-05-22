867d2c00858d25fa8873bc84af90c7991e5c594a
guobin <18500056398@163.com>
2018-05-23 00:24:51 +0800

    Implement Rasterize2DLine function
    Implement Rasterize2DLine function with Bresenham's line algorithm.

--- src/SoftwareRenderer.cpp
+++ src/SoftwareRenderer.cpp
@@ -41,26 +41,101 @@ namespace g3dcommon
     {
       scene->Render(this);
     }
+
+    Rasterize2DLine(260, 640, 820, 120,Color(1.f,0.f,0.f,1.f));
   }
 
   void SoftwareRenderer::Rasterize2DPoint(float x, float y, Color color)
   {
     int sx = static_cast<int>(floor(x));
-    int sy = static_cast<int>(floor(y));
+    int sy = static_cast<int>(floor(targetHeight - 1 - y));
     if (sx < 0 || sx >= static_cast<int>(targetWidth) || sy < 0 || sy >= static_cast<int>(targetHeight))
     {
       return;
     }
     int index = sy * targetWidth + sx;
     renderTarget[index * 4] = color.R();
-    renderTarget[index + 1] = color.G();
-    renderTarget[index + 2] = color.B();
-    renderTarget[index + 3] = color.A();
+    renderTarget[index * 4  + 1] = color.G();
+    renderTarget[index * 4  + 2] = color.B();
+    renderTarget[index * 4  + 3] = color.A();
   }
 
   void SoftwareRenderer::Rasterize2DLine(float x0, float y0, float x1, float y1, Color color)
   {
+    int xStart = static_cast<int>(x0);
+    int yStart = static_cast<int>(y0);
+    int xEnd = static_cast<int>(x1);
+    int yEnd = static_cast<int>(y1);
 
+    bool steep = fabs(yEnd - yStart) > fabs(xEnd - xStart);
+    if (steep)
+    {
+      std::swap(xStart, yStart);
+      std::swap(xEnd, yEnd);
+    }
+    if (xStart > xEnd)
+    {
+      std::swap(xStart, xEnd);
+      std::swap(yStart, yEnd);
+    }
+    int x = xStart;
+    int y = yStart;
+    if (xStart == xEnd)
+    {
+      while (y <= yEnd)
+      {
+        Rasterize2DPoint(x, y, color);
+        y++;
+      }
+    }
+    else
+    {
+      int ystep = yStart < yEnd ? 1 : -1;
+      int dx = xEnd - xStart;
+      int dy = yEnd - yStart;
+      int twoDy = 2 * dy;
+      int twoDyMinusDx = 2 * (dy - dx);
+      int twoDyPlusDx = 2 * (dy + dx);
+      int p = twoDy - ystep * dx;
+      while (x <= xEnd)
+      {
+        if (steep)
+        {
+          Rasterize2DPoint(y, x, color);
+        }
+        else
+        {
+          Rasterize2DPoint(x, y, color);
+        }
+
+        x++;
+
+        if (p < 0)
+        {
+          if (yStart < yEnd)
+          {
+            p += twoDy;
+          }
+          else
+          {
+            p += twoDyPlusDx;
+            y--;
+          }
+        }
+        else
+        {
+          if (yStart < yEnd)
+          {
+            p += twoDyMinusDx;
+            y++;
+          }
+          else
+          {
+            p += twoDy;
+          }
+        }
+      }
+    }
   }
 
 
